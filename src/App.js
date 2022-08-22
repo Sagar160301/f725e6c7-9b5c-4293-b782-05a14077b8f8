d7ad5d67a71fd73430e189c2f6f3a4cbd3b61bb5
Sagar <94003978+Sagar160301@users.noreply.github.com>
2022-08-22 23:46:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2 data-testid="counter-value">X</h2>
+      <button data-testid="counter-decrement-button"  ></button>
+      <button data-testid="counter-increment-button" ></button>
+    </div>
+  );
+}
+
+export default App;
